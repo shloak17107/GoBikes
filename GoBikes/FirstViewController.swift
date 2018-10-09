3a23118b65844e770fd333f22c33e82851fbf30f
Shloak <31565271+shloak17107@users.noreply.github.com>
2018-10-10 00:40:12 +0530

    Initial Commit

---
+++ GoBikes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GoBikes
+//
+//  Created by Shloak Aggarwal on 10/10/18.
+//  Copyright © 2018 GoBikes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GoBikes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GoBikes
+//
+//  Created by Shloak Aggarwal on 10/10/18.
+//  Copyright © 2018 GoBikes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
